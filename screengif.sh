bd6a75784461c406bd3032d2547daff59f61a1f2
Brian Feister <47546998+bfeister@users.noreply.github.com>
2024-11-19 06:58:55 -0700

    Update to latest automator script + support brew in both intel + apple silicon chip architectures

--- screengif.sh
+++ screengif.sh
@@ -1,10 +1,48 @@
-if [ "$1" = "" ] ;
-then
-	echo "Please pass a QuickTime movie file."
-else
-	export fspec="$1"
-	fname=`basename $fspec`
-	fname="${fname%.*}"
-	filedir=$(dirname "$1")
-	/usr/local/bin/ffmpeg -i "$1" -pix_fmt rgb24 -r 10 -f gif - | /usr/local/bin/gifsicle --optimize=3 --delay=1 --resize-touch=800x800 > $filedir/$fname.gif
-fi
+#!/bin/bash
+
+PATH="$PATH:/usr/local/bin/:/opt/homebrew/bin/"
+
+commandCheck() {
+    cmd="$1"
+    command -v "${cmd}" >/dev/null 2>&1 || { echo >&2 "I require ${cmd} but it's not installed. Aborting. brew install ${cmd}"; exit 1; }
+}
+
+commandCheck gifsicle
+commandCheck ffmpeg
+
+find "$@" -type f -name '*.mov' -print0 | while IFS= read -r -d '' f
+do
+    # base
+    export fspec="$f"
+    basefile="${fspec// /_}"
+    basefile=$(basename "${basefile}")
+    basefile="${basefile%.*}"
+
+    # dirs
+    filedir=$(dirname "${f}")
+    tmpdir="/tmp"
+
+    # output files
+    palette="$tmpdir/$basefile-palette.png"
+    tmpgif="$tmpdir/$basefile.gif"
+    output="$filedir/$basefile.gif"
+
+    if [[ -f "${output}" ]]; then
+        echo "Skipping $f"
+    else
+        echo "Converting $f -> ${output}"
+
+        # gif config
+        maxsize=700
+        filters="fps=8,scale=${maxsize}:-1:flags=lanczos"
+
+        # create
+        ffmpeg -v warning -i "$f" -vf "$filters,palettegen" -y "$palette"
+        ffmpeg -v warning -i "$f" -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y "$tmpgif"
+        gifsicle --optimize=3 --delay=1 --loopcount=forever --resize-touch="${maxsize}x${maxsize}" "$tmpgif" > "$output"
+
+        # cleanup
+        rm $palette
+        rm $tmpgif
+    fi
+done
